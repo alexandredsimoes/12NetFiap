295f3d17e005dababc00a009f9b1ac59ba6b4a9b
Alexandre Dias <alexandre.dias.simoes@outlook.com>
2018-03-24 13:58:16 -0300

    Alteracoes

---
+++ DevWeb/Aulas/TesteConsole/ViewComponents/TesteComponente.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteConsole.ViewComponents
+{
+    public class TesteComponente : ViewComponent
+    {
+        public TesteComponente()
+        {
+
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            var obj = new
+            {
+                Id = 10,
+                Nome = "Alexandre Dias"
+            };
+
+            return Task.FromResult<IViewComponentResult>(View(viewName: "Teste", model: obj));
+        }
+    }
+}
