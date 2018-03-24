5031bcee86f10efbb401d9c0221ce318f7f7e448
logonrm <logonrm@fiap.com.br>
2018-03-24 10:44:13 -0300

    adicionado trabalho

---
+++ DevWeb/Trabalho/MVC-Copa-Do-Mundo/CopaDoMundo.Models/CopaContexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CopaDoMundo.Models
+{
+    public class CopaContexto : DbContext
+    {
+        public DbSet<Time> Times { get; set; }       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {            
+            optionsBuilder.UseSqlite("Filename=Copa.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Time>()
+                .Property(c => c.Nome)
+                .HasMaxLength(100);
+        }
+    }
+}
---
+++ DevWeb/Trabalho/MVC-Copa-Do-Mundo/CopaDoMundo.Models/Time.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopaDoMundo.Models
+{
+    public class Time
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataFundacao { get; set; }
+    }
+}
